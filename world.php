1566c65bfe1a5686d797f80cc7e312875511fc84
Camille Campbell <c.campbell1996@hotmail.com>
2016-11-18 03:32:29 +0000

    Changes from exercise 4

--- world.php
+++ world.php
@@ -1,12 +1,22 @@
 <?php
-
 $host = getenv('IP');
-$country = $_GET['country'];
+$country = $_GET['all'];
 $username = getenv('C9_USER');
 $password = '';
 $dbname = 'world';
 $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+if($country=="")
+{
+   $stmt = $conn->query("SELECT * FROM countries WHERE 1 = 0");
+}
+else if($country=="true")
+{
+    $stmt = $conn->query("SELECT * FROM countries");
+}
+else
+{
+    $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+}
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 echo '<ul>';
 foreach ($results as $row) {
